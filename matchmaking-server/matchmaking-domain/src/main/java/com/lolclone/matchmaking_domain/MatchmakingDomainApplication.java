bbdfa5f507b29e0152cf949d6f3f25b3d7b82099
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2025-01-09 18:16:03 +0900

    chore: 매칭 도메인 모듈 설정 변경  - build.gradle 설정 추가   - QueryDSL 설정 추가   - Common 모듈 의존성 추가   - bootJar, jar 설정 추가   - clean task에 generated 폴더 삭제 추가  - 불필요한 파일 제거   - MatchmakingdomainApplication.java 삭제   - application.properties 삭제

--- matchmaking-server/matchmaking-domain/src/main/java/com/lolclone/matchmaking_domain/MatchmakingDomainApplication.java
+++ matchmaking-server/matchmaking-domain/src/main/java/com/lolclone/matchmaking_domain/MatchmakingDomainApplication.java
@@ -1,13 +1,13 @@
-package com.loclone.matchmakingdomain;
+package com.lolclone.matchmaking_domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatchmakingdomainApplication {
+public class MatchmakingDomainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatchmakingdomainApplication.class, args);
+		SpringApplication.run(MatchmakingDomainApplication.class, args);
 	}
 
 }
